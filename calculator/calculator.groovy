4fe93932ceb2e496df17013ccb625a44c9607186
Luis Barbosa <lbarbosa@daitangroup.com>
2017-09-04 10:33:23 -0300

    Pushing basic operations

---
+++ calculator/calculator.groovy
@@ -0,0 +1,30 @@
+
+println "Calculator"
+
+def n1 = 10
+def n2 = 3
+def total
+def res
+
+total = n1+n2
+res = n1 + " + " + n2 + " = " + total
+println res
+
+total = n1-n2
+res = n1 + " - " + n2 + " = " + total
+println res
+
+total = n1*n2
+res = n1 + " * " + n2 + " = " + total
+println res
+
+total = n1/n2
+res = n1 + " / " + n2 + " = " + total
+println res
+
+total = n1%n2
+res = n1 + " % " + n2 + " = " + total
+println res
+
+
+
